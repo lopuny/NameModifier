24c5381d87e9b6dbe8da3fe4fdd80491fa4176d3
lopuny <50102852+lopuny@users.noreply.github.com>
2021-08-13 14:06:02 +0800

    add name dictionary

---
+++ name_dic.py
@@ -0,0 +1,301 @@
+
+##### 1 ##### BH_RR_RB211 #####
+
+BH_RR_RB211 = {
+# External Condition: 
+"0T": 0,
+"0GVAa": "GGJGVF1",
+"0GVAb": "GGJGVF2",
+"0TFa": "A26FGRA",
+"0TFb": "A26FGRB",
+"0TFc": "A26FGRC",
+"0TFd": "A26FGRD",
+"0PF": "A63FGM",
+
+# Measurements:
+"2Ta": "A26GG25A",
+"2Tb": "A26GG25B",
+"3Ta": "A26GG31A",
+"3Tb": "A26GG31B",
+"5Ta": "A26GG05A",
+"5Tb": "A26GG05B",
+"5Tc": "A26GG05C",
+"6Ta": "A26GG455B",
+"6Tb": "GG45501A",
+"6Tc": "GG45502A",
+"6Td": "GG45503A",
+"6Te": "GG45504A",
+"6Tf": "GG45505A",
+"6Tg": "GG45506A",
+"6Th": "GG45507A",
+"6Ti": "GG45508A",
+"6Tj": "GG45509A",
+"6Tk": "GG45510A",
+"6Tl": "GG45511A",
+"6Tm": "GG45512A",
+"6Tn": "GG45513A",
+"6To": "GG45514A",
+"6Tp": "GG45515A",
+"6Tq": "GG45516A",
+"6Tr": "GG45517A",
+"3V": "A39GGC",
+"4V": "A39GGI",
+"5V": "A39GGT",
+"1P": "A63GG20",
+"2P": "A63GG25",
+"6P": "A63GG455",
+"NLa": "A99GGNL1",
+"NLb": "A99GGNL2",
+"NHa": "A99GGNH1",
+"NHb": "A99GGNH2",
+"NPa": "A99PT1", 
+"NPb": "A99PT2",
+
+"7T_2a": "A26PTRC1A",
+"7T_2b": "A26PTRC1B",
+"7T_4a": "A26PTRC2A",
+"7T_4b": "A26PTRC2B",
+"TS": "A26EVGTA",
+}
+
+BH_RR_RB211_error = {     # 可能出现的错误名称 
+"3Tb": "A26GG311B", 
+}
+
+BH_RR_RB211_avg = [
+# External Condition: 
+"0GVA",
+"0TF",
+
+# Measurements:
+"2T",
+"3T",
+"5T",
+"6T",
+"NL",
+"NH",
+"NP",
+
+"7T_2",
+"7T_4",
+]
+
+##### 2 ##### BH_GE_LM2500 #####
+
+BH_GE_LM2500 = {
+# External Condition: 
+"0PF": "gp2b",
+"0TF": "ftg2b",
+"0FF": "A60FG6W",
+"0T": "a26at",
+"0GVAa": "HWINVSVA",
+"0GVAb": "HWINVSVAA",
+"0GVAc": "VSVPOPDM",
+"1T": "T2A",
+
+
+# Measurements:
+"1P": "P2SEL",
+"3P": "PS3A",
+"3PS": "PS3B",
+"4P": "p48",
+"6P": "A63EP",
+"3Ta": "T3A1",
+"3Tb": "T3A2",
+"3Tc": "T3B1",
+"3Td": "T3B2",
+"6TOa": "A26BT",
+"6TOb": "a26bt1", 
+"6TOc": "a26bt2", 
+"6Ta": "T48A",
+"6Tb": "T48B",
+"6Tc": "T48C",
+"6Td": "T48D",
+"6Te": "T48E",
+"6Tf": "T48F",
+"6Tg": "T48G",
+"6Th": "T48H",
+# "6Ti": "T48SEL",
+
+"NHa": "NGGA",
+"NHb": "NGGB",
+"NPa": "NPTA",
+"NPb": "NPTB",
+
+"7Ta": "T8a",
+"7Tb": "T8b",
+"7Tc": "T8c",
+"7Td": "T8d",
+"7Te": "T8e",
+"7Tf": "T8f",
+"7T_1a": "ws1fia",
+"7T_1b": "ws1fib",
+"7T_2a": "ws1aia",
+"7T_2b": "ws1aib",
+"7T_3a": "ws2fia",
+"7T_3b": "ws2fib",
+"7T_4a": "ws2aia",
+"7T_4b": "ws2aib",
+}
+
+BH_GE_LM2500_error = {     # 可能出现的错误名称 
+}
+
+BH_GE_LM2500_avg = [
+# External Condition:
+"0GVA",
+
+# Measurements:
+"6TO",
+"3T",
+"6T",
+"NH",
+"NP",
+"7T",
+"7T_1",
+"7T_2",
+"7T_3",
+"7T_4",
+]
+
+##### 3 ##### Liburdi_RR_RB211 #####
+
+Liburdi_RR_RB211 = {
+# External Condition: 
+"0H": "humidity",
+"0T": "26am",
+"0P": "barometric",
+"0TF": "a26fgra",
+"0PF": "a63fgr",
+"0GVA": "c75ggigvc",
+"0OPT": "optime",
+
+# Measurements:
+"6Ta": "a26gg455b",
+"6Tb": "a26gg455.01a",
+"6Tc": "a26gg455.02a",
+"6Td": "a26gg455.03a",
+"6Te": "a26gg455.04a",
+"6Tf": "a26gg455.05a",
+"6Tg": "a26gg455.06a",
+"6Th": "a26gg455.07a",
+"6Ti": "a26gg455.08a",
+"6Tj": "a26gg455.09a",
+"6Tk": "a26gg455.10a",
+"6Tl": "a26gg455.11a",
+"6Tm": "a26gg455.12a",
+"6Tn": "a26gg455.13a",
+"6To": "a26gg455.14a",
+"6Tp": "a26gg455.15a",
+"6Tq": "a26gg455.16a",
+"6Tr": "a26gg455.17a",
+"6P": "p6",
+"3P": "a63gg30",
+"NLa": "a99ggnl1",
+"NLb": "a99ggnl2",
+"NHa": "a99ggnh1",
+"NHb": "a99ggnh2",
+"NPa": "a99pt1",
+"NPb": "a99pt2",
+"2P": "p25",
+"7P": "ptexh.pressure",
+"2T": "t25",
+"3T": "t3",
+}
+
+Liburdi_RR_RB211_error = {    
+}
+
+Liburdi_RR_RB211_avg = [
+# Measurements:
+"6T",
+"NL",
+"NH",
+"NP",
+]
+
+
+##### 4 ##### Liburdi_GE_LM2500 #####
+
+Liburdi_GE_LM2500 = {
+# External Condition: 
+"0P": "barometric",
+"0T": "a26at",
+"0H": "a90rh",
+"0TF": "a26fg",
+"0OPT": "optime",
+"0PF": "a63gs",
+"0FF": "a60fg6",
+"1Ta": "t2sel",
+"1Tb": "t2a",
+"1Tc": "t2b",
+"0LOAD": "gtapload",
+
+# Measurements:
+"1Pa": "a96tf1", 
+"1Pb": "p2a",
+"6Ta": "t48a",
+"6Tb": "t48b",
+"6Tc": "t48c",
+"6Td": "t48d",
+"6Te": "t48e",
+"6Tf": "t48f",
+"6Tg": "t48g",
+"6Th": "t48h",
+# "6Ti": "t48sel",
+"6P": "a63hp",
+"NHa": "ngg.a",
+"NHb": "ngg.b",
+# "NHc": "ngg.sel",
+"3PSa": "ps3a",
+"3PSb": "ps3b",
+# "3PSc": "ps3sel",
+"3Ta": "t3sel",
+"3Tb": "t3a1",
+"3Tc": "t3a2",
+"3Td": "t3b1",
+"3Te": "t3b2",
+
+"NPa": "npt.a",
+"NPb": "npt.b",
+# "NPc": "npt.sel",
+"7Ta": "t8a",
+"7Tb": "t8b",
+"7Tc": "t8c",
+"7Td": "t8d",
+"7Te": "t8e",
+"7Tf": "t8f",
+"7Tg": "t8g",
+"7Th": "t8h",
+# "7Ti": "t8sel",
+"7T_1a": "ws1ai.a",
+"7T_1b": "ws1ai.b",
+"7T_2a": "ws1fi.a",
+"7T_2b": "ws1fi.b",
+"7T_3a": "ws2ai.a",
+"7T_3b": "ws2ai.b",
+"7T_4a": "ws2fi.a",
+"7T_4b": "ws2fi.b",
+"7P": "ptexh.pressure",
+}
+
+Liburdi_GE_LM2500_error = {
+}
+
+Liburdi_GE_LM2500_avg = [
+# External Condition:
+"1T",
+
+# Measurements:
+"1P",
+"3T",
+"6T",
+"NH",
+"NP",
+"3PS",
+"7T",
+"7T_1",
+"7T_2",
+"7T_3",
+"7T_4",
+]
